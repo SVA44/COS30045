dee45792477a27b41f4cc17a027dcfa62c6f05d5
VLa24 <123402491+SVA44@users.noreply.github.com>
2024-04-22 09:02:52 +0700

    Add files via upload

--- lab7/lab7.1.js
+++ lab7/lab7.1.js
@@ -14,67 +14,67 @@ function init() {
     }).then(function(data) {
         dataset = data;
         console.table(dataset, ["date", "number"]);
-        // lineChart(dataset);
+        lineChart(dataset);
     })
 
-    // function lineChart(dataset) {
-    //     var svg = d3.select("#chart")
-    //             .append("svg")
-    //             .attr("width", w)
-    //             .attr("height", h)
+    function lineChart(dataset) {
+        var svg = d3.select("#chart")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h)
 
-    //     xScale = d3.scaleTime()
-    //                .domain([
-    //                     d3.min(dataset, function(d) { return d.date; }),
-    //                     d3.max(dataset, function(d) { return d.date; })
-    //                ])
-    //                .range([padding, w]);
+        xScale = d3.scaleTime()
+                   .domain([
+                        d3.min(dataset, function(d) { return d.date; }),
+                        d3.max(dataset, function(d) { return d.date; })
+                   ])
+                   .range([padding, w]);
 
-    //     yScale = d3.scaleLinear()
-    //                .domain([0, d3.max(dataset, function(d) { return d.number })])
-    //                .range([h - padding, 0]);
-    //     area = d3.area()
-    //              .x(function(d) { return xScale(d.date); })
-    //              .y0(function() { return yScale.range()[0]; })
-    //              .y1(function(d) { return yScale(d.number); })
+        yScale = d3.scaleLinear()
+                   .domain([0, d3.max(dataset, function(d) { return d.number })])
+                   .range([h - padding, 0]);
+        area = d3.area()
+                 .x(function(d) { return xScale(d.date); })
+                 .y0(function() { return yScale.range()[0]; })
+                 .y1(function(d) { return yScale(d.number); })
 
-    //     var xAxis = d3.axisBottom()
-    //              .ticks(5)
-    //              .scale(xScale);
+        var xAxis = d3.axisBottom()
+                 .ticks(5)
+                 .scale(xScale);
 
-    //     var yAxis = d3.axisLeft()
-    //              .ticks(5)
-    //              .scale(yScale);   
+        var yAxis = d3.axisLeft()
+                 .ticks(5)
+                 .scale(yScale);   
             
-    //     svg.append("path")
-    //        .datum(dataset)
-    //        .attr("class", "area")
-    //        .attr("d", area)
+        svg.append("path")
+           .datum(dataset)
+           .attr("class", "area")
+           .attr("d", area)
 
-    //     svg.append("line")
-    //        .attr("class", "line halfMilMark")
-    //        // start of line
-    //        .attr("x1", padding)
-    //        .attr("y1", yScale(500000))
-    //        // end of line
-    //        .attr("x2", w)
-    //        .attr("y2", yScale(500000))
+        svg.append("line")
+           .attr("class", "line halfMilMark")
+           // start of line
+           .attr("x1", padding)
+           .attr("y1", yScale(500000))
+           // end of line
+           .attr("x2", w)
+           .attr("y2", yScale(500000))
         
-    //     svg.append("text")
-    //        .attr("class", "halfMilLabel")
-    //        .attr("x", padding + 10)
-    //        .attr("y", yScale(500000) - 7)
-    //        .text("Half a million unemployed");
+        svg.append("text")
+           .attr("class", "halfMilLabel")
+           .attr("x", padding + 10)
+           .attr("y", yScale(500000) - 7)
+           .text("Half a million unemployed");
 
-    //     svg.append("g")
-    //        .attr("transform", "translate(0, " + (h - padding) + ")")
-    //        .call(xAxis);
+        svg.append("g")
+           .attr("transform", "translate(0, " + (h - padding) + ")")
+           .call(xAxis);
 
-    //     svg.append("g")
-    //        .attr("transform", "translate(" + (padding) + ", 0)")
-    //        .call(yAxis);
+        svg.append("g")
+           .attr("transform", "translate(" + (padding) + ", 0)")
+           .call(yAxis);
 
                    
-    // }
+    }
 }
 window.onload = init;
